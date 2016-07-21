53d5beb800907f92513fbe3c0977e5ef0c440159
Tim Beurskens <tim.beurskens@factory97.com>
2016-07-21 23:58:56 +0200

    fix concept of listitem module

--- demos/modules/listitem/listitem.js
+++ demos/modules/listitem/listitem.js
@@ -1,10 +1,12 @@
-modules.export = {
-  /***
-  * The initialize function gets called after the project tree is built.
-  * The module_id variable contains the current unique id of this module instance,
-  * it can be used to communicate with other modules in the tree using the getModuleById(module_id) function.
-  ***/
-  initialize: function(module_id){
+/***
+* This function gets called after the project tree is built.
+* The module_id variable contains the current unique id of this module instance,
+* it can be used to communicate with other modules in the tree using the getModuleById(module_id) function.
+***/
+module.exports = function(module_id){
+  this.test = function(){
+    return module_id;
+  };
 
-  }
+  return this;
 };
