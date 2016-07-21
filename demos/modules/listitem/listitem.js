70c5300480e131edc7277489578ab973675cbd9c
Tim Beurskens <tim.beurskens@factory97.com>
2016-07-21 21:33:13 +0200

    Add sample module

---
+++ demos/modules/listitem/listitem.js
@@ -0,0 +1,10 @@
+modules.export = {
+  /***
+  * The initialize function gets called after the project tree is built.
+  * The module_id variable contains the current unique id of this module instance,
+  * it can be used to communicate with other modules in the tree using the getModuleById(module_id) function.
+  ***/
+  initialize: function(module_id){
+
+  }
+};
