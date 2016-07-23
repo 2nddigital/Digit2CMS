b5e50804c267c064039005445bfb71043eb40c7a
Tim Beurskens <tim.beurskens@factory97.com>
2016-07-23 16:16:14 +0200

    implement basic tree behaviour

--- src/core/module.js
+++ src/core/module.js
@@ -1,7 +1,25 @@
-module.exports = function(moduleObject){
+function Module(moduleObject){
   var self = this;
-  var containers = {};
+  this.containers = {};
 
+  // Load containers from moduleObject into containers object, initialize with null
+  if(typeof(moduleObject.containers) === 'object'){
+    moduleObject.containers.forEach(function(containerId){
+      self.containers[containerId] = null;
+    });
+  }
 
   return this;
+}
+
+Module.prototype.setChild = function(containerId, childModule){
+  if(childModule instanceof Module && typeof(this.containers[containerId]) !== 'undefined'){
+    this.containers[containerId] = childModule;
+  }
 };
+
+Module.prototype.getChild = function(containerId){
+  return (typeof(this.containers[containerId]) !== 'undefined') ? this.containers[containerId] : null;
+};
+
+module.exports = Module;
