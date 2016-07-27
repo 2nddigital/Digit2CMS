a11f67ba4adb0ac5f819a39bbe930b935e84e025
Tim Beurskens <tim.beurskens@factory97.com>
2016-07-27 12:29:49 +0200

    create new extend function

--- src/core/proto.js
+++ src/core/proto.js
@@ -6,3 +6,18 @@ Object.prototype.extend = function(ext){
   }
   return this;
 };
+
+Object.prototype.safeExtend = function(ext){
+  var newObject = {};
+  for(var i1 in this){
+    if(this.hasOwnProperty(i1)){
+      newObject[i1] = this[i1];
+    }
+  }
+  for(var i2 in ext){
+    if(ext.hasOwnProperty(i2)){
+      newObject[i2] = ext[i2];
+    }
+  }
+  return newObject;
+};
